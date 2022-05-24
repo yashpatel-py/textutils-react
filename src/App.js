205ba0eed978182fa25319ce068facbcc16d8c02
Yash Patel <mailforyash04@gmail.com>
2022-05-24 16:56:27 +0000

    Added some dark mode code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import AboutUs from './components/AboutUs'
 
 function App() {
+  const [mode, setMode] = useState('light')
   return (
     <>
       {/* Importing navbar component */}
-      <Navbar title = "TU" aboutText= "About us" />
+      <Navbar title = "TU" aboutText= "About us" mode={mode}/>
 
       {/*Importing TextForm component */}
       <div className="container">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -54,7 +54,7 @@ const MaterialUISwitch = styled(Switch)(({theme}) => ({
 }));
 export default function Navbar(props) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
